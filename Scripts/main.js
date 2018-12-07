6e74f212bc9de10ef4303310891edf2fcbb33ec8
Sami Hamdan <samihamdan00@gmail.com>
2018-12-07 10:55:51 -0500

    split news page off, added animations and splash page

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,6 +1,12 @@
 $(document).ready(function(){
   //insert logo click and hide body, reveal after Clicker
 
+  $('.content').hide();
+
+  $('.splash-logo').click(function(){
+    $('.splash-logo').hide(500);
+    $('.content').show(1000);
+  });
 
   $('.dropdown').on({
     mouseenter: function(){
