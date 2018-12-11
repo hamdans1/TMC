9cb03a432a505a04fbe85ab2168619b9033885bf
Sami Hamdan <samihamdan00@gmail.com>
2018-12-11 03:21:16 -0500

    more frontpage art

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,13 +1,4 @@
 $(document).ready(function(){
-  //insert logo click and hide body, reveal after Clicker
-
-  /* $('.content').hide();
-
-  $('.splash-logo').click(function(){
-    $('.splash-logo').hide(500);
-    $('.content').show(1000);
-  });
-  */
 
   $('.dropdown').on({
     mouseenter: function(){
